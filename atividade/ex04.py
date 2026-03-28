9238093552c33dfc9d87cff7c8919218c792dc7c
Daniel Pinto de França Barros <daniellpinto_@outlook.com>
2026-03-28 12:31:53 -0300

    Atualizar o ex04.py

--- atividade/ex04.py
+++ atividade/ex04.py
@@ -1,4 +1,4 @@
 num1 = float(input("Digite um número: "))
 num2 = float(input("Digite outro número: "))
 sub = num1 - num2
-print(f"O valor da subtração é {sub}")
+print("O valor da subtração é", sub)
