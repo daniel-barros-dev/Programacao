0a99592ccd75f1a9584b7c7fa4f11b9b6f733d51
Daniel Pinto de França Barros <daniellpinto_@outlook.com>
2026-03-31 16:43:26 -0300

    Atividade

--- Segunda atividade/exerc07.py
+++ Segunda atividade/exerc07.py
@@ -1,4 +1,4 @@
-#Ver se o número é maior que 10 ou menor ou igual a 10
+#Ver se o número é maior que 10 e menor ou igual a 10
 num = float(input("Digite um número: "))
 if num > 10:
     print("O número", num,"é maior do que 10")
